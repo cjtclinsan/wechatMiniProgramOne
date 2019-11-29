77e8400ccaf3a2e0d7276e30629f70b33fb90b97
shent <shent@163.com>
2019-11-29 18:51:26 +0800

    1129

--- pages/wxss/wxss.js
+++ pages/wxss/wxss.js
@@ -5,62 +5,51 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    message: 123,
+    firstName: 'kobe',
+    lastName: 'bryant',
+    nowTime: new Date().toLocaleString(),
+    isActive: true,
+    isShow: true,
+    show: '隐藏',
+    score: 70,
+    movies: [
+      '大话西游','泰坦尼克','罗密欧'
+    ],
+    nums: [
+      [1,2,3],
+      [4,5,6],
+      [7,8,9]
+    ],
+    letters: [
+      'a','b','c'
+    ]
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad() {
+    setInterval(() => {
+      this.setData({
+        nowTime :new Date().toLocaleString()
+      })
+    }, 1000)
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleSwitchColor(){
+    console.log(this.data.isActive)
+    this.setData({
+      isActive: !this.data.isActive
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleSwitchShow(){
+    this.setData({
+      isShow: !this.data.isShow
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleIncrement(){
+    this.setData({
+      score: this.data.score+5
+    })
   }
 })
